825a6de840565f91cdf9eae70f50ba9224a88c3b
Lương Nguyễn Khoa <107914230+CRLLNKhoa@users.noreply.github.com>
2023-11-14 16:30:47 +0700

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,7 +6,7 @@ import React, { ReactNode } from "react";
 function Layout({ children }: { children: ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
-      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2 sticky top-0">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2 sticky top-0 bg-background">
         <Logo />
         <ThemeSwitcher />
         <UserButton afterSignOutUrl="/sign-in" />
